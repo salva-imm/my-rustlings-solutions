ffa2d142e9e6873d6e947049bd2503024051c50e
salva-imm <mehdi73ee@gmail.com>
2024-03-03 03:10:03 +0330

    part 1

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,11 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Debug)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
@@ -39,6 +39,24 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        scores.entry(team_1_name).and_modify(|t1| {
+            t1.goals_scored += team_1_score;
+            t1.goals_conceded += team_2_score;
+        })
+            .or_insert(
+                Team{
+                    goals_scored: team_1_score,
+                    goals_conceded: team_2_score
+            });
+        scores.entry(team_2_name).and_modify(|t2| {
+            t2.goals_scored += team_2_score;
+            t2.goals_conceded += team_1_score;
+        })
+            .or_insert(
+                Team{
+                    goals_scored: team_2_score,
+                    goals_conceded: team_1_score
+                });
     }
     scores
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,12 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
 
-// Don't modify this function!
+fn calculate_price_of_apples(num: i32) -> i32{
+    if num < 41 {
+        num * 2
+    }else {
+        num
+    }
+}
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
